b483c2e11822e89b766b200c6ca6dc6e22e1619b
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2025-02-08 01:52:09 +0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,9 @@ function App() {
   return (
     <div className='flex w-full h-screen justify-center items-center gap-5 bg-[#26272b]  '>
       <Button styled={true} content={btnNumber == 'one' && "button one clicked"} func={handlebtnone}  />
-      <Button styled={true} content={btnNumber == 'two' && "button two clicked"} func={handlebtntwo} />
-      <Button  content={btnNumber == 'three' && "button three clicked"} func={handlebtnthree}/>
-      <Button styled={true} content={btnNumber == 'four' && "button four clicked"} func={handlebtnfour}/>
+      <Button styled={btnNumber == 'two' ? false : true} content={btnNumber == 'two' && "button two clicked"} func={handlebtntwo} />
+      <Button styled={true} content={btnNumber == 'three' && "button three clicked"} func={handlebtnthree}/>
+      <Button styled={btnNumber == 'four' ? false : true} content={btnNumber == 'four' && "button four clicked"} func={handlebtnfour}/>
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Button = ({ content , func , styled}) => {
+  console.log(content);
+  console.log(styled);
+  
   return (
-    <button onClick={func} type='button' className={` ${styled ? 'button' : ' font-bold text-white uppercase ' }   cursor-pointer`}> {content ? content : 'button'}</button>
+    <button onClick={func} type='button' className={` ${styled ? 'button' : ' font-bold text-white uppercase ' }   cursor-pointer`}> 
+    {/* { content && styled ? content : `button`} */}
+    { !content && styled && 'button'}
+    { content && styled && content}
+    {!styled && !content && 'button'}
+    {!styled && content && 'style  removed'}
+    </button>
   )
 }
 
